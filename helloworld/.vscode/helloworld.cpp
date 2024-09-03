a0b2046a029b6886e5f6f28517561d441404d62f
Ameliathedeveloper <millzds@gmail.com>
2024-09-03 17:31:46 +0100

    still playing around

--- helloworld/.vscode/helloworld.cpp
+++ helloworld/.vscode/helloworld.cpp
@@ -1,24 +1,23 @@
-#include <iostream>
+#include <iostream> //this header file provides definitions for input and output streams. 
 #include <vector>
 #include <string>
 
-using namespace std;
 
-int main()
-{
-   cout << "Amelia Davies-Smith"; 
-  return 0; 
-   
-   
-   
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-    
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
-}
 
 //my first comment
+
+int main() 
+
+/* The main() is the main function that houses your program. This is where we will be writing our code.
+
+Every C++ program must have a function called main(). A function is basically a sequence of instructions for the computer to execute. */
+
+
+{
+   std::cout << "A \n";
+   std::cout << "J \n";
+   std::cout << "D \n";
+   std::cout << "S \n";
+   
+}
+
